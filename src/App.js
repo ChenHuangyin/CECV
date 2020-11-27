ed104d73a16b33028fbc31f98f3e386af718de4f
Squarepants <chenhy98@mail2.sysu.edu.cn>
2020-11-27 04:06:27 -0500

    code style fixed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-
-import React, { Component } from 'react';
-import { BECIEstimated, BECIMinimum } from './data/BECI';
-import { EECIEstimated, EECIMinimum } from './data/EECI';
-import ECI from './ECI';
-import * as _ from 'lodash';
-import './App.css';
+import React, { Component } from "react";
+import { BECIEstimated, BECIMinimum } from "./data/BECI";
+import { EECIEstimated, EECIMinimum } from "./data/EECI";
+import ECI from "./ECI";
+import * as _ from "lodash";
+import "./App.css";
 
 class App extends Component {
   constructor(props) {
@@ -13,7 +12,7 @@ class App extends Component {
       selected: 1,
       estimatedData: _.cloneDeep(BECIEstimated),
       minimumData: _.cloneDeep(BECIMinimum),
-      title: 'Bitcoin Energy Consumption Index'
+      title: "Bitcoin Energy Consumption Index",
     };
   }
 
@@ -22,23 +21,28 @@ class App extends Component {
       selected: 1,
       estimatedData: _.cloneDeep(BECIEstimated),
       minimumData: _.cloneDeep(BECIMinimum),
-      title: 'Bitcoin Energy Consumption Index'
+      title: "Bitcoin Energy Consumption Index",
     });
-  }
+  };
 
   EECISelected = () => {
     this.setState({
       selected: 2,
       estimatedData: _.cloneDeep(EECIEstimated),
       minimumData: _.cloneDeep(EECIMinimum),
-      title: 'Ethereum Energy Consumption Index'
+      title: "Ethereum Energy Consumption Index",
     });
-  }
+  };
 
   render() {
     let content;
-    content = <ECI estimatedData={this.state.estimatedData} minimumData={this.state.minimumData} title={this.state.title} />;
-    
+    content = (
+      <ECI
+        estimatedData={this.state.estimatedData}
+        minimumData={this.state.minimumData}
+        title={this.state.title}
+      />
+    );
     return (
       <div className="App">
         <div className="App-header">
@@ -46,8 +50,26 @@ class App extends Component {
         </div>
         <div className="container">
           <div className="nav">
-            <button type='button' className={"btn btn-highchart " + (this.state.selected === 1 ? 'active' : '')} onClick={this.BECISelected}>BECI</button>
-            <button type='button' className={"btn btn-highchart " + (this.state.selected === 2 ? 'active' : '')} onClick={this.EECISelected}>EECI</button>
+            <button
+              type="button"
+              className={
+                "btn btn-highchart " +
+                (this.state.selected === 1 ? "active" : "")
+              }
+              onClick={this.BECISelected}
+            >
+              BECI
+            </button>
+            <button
+              type="button"
+              className={
+                "btn btn-highchart " +
+                (this.state.selected === 2 ? "active" : "")
+              }
+              onClick={this.EECISelected}
+            >
+              EECI
+            </button>
           </div>
           {content}
         </div>
